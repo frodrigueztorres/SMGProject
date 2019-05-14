5117376b2b91005ba308d679139261ff46774036
frodrigueztorres <felipe.andres.rodt@gmail.com>
2019-05-13 21:50:41 -0400

    Login interface

---
+++ app/src/main/java/com/example/felip/smgproyect/IntroMenu.java
@@ -0,0 +1,15 @@
+package com.example.felip.smgproyect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_menu);
+
+        //Insertar codigo
+    }
+}
