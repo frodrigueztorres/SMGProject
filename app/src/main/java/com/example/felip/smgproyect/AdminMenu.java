08d5dbcfa2ced109ee1f6dd856d85ce14a6f2587
frodrigueztorres <felipe.andres.rodt@gmail.com>
2019-06-16 19:45:10 -0400

    Set all interfaces to Fullscreen

--- app/src/main/java/com/example/felip/smgproyect/AdminMenu.java
+++ app/src/main/java/com/example/felip/smgproyect/AdminMenu.java
@@ -2,12 +2,26 @@ package com.example.felip.smgproyect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminMenu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
     }
 }
